782ddc9a46812087072eaa61a0a62c35aceaa035
Muhdo <gabrielmcv1@gmail.com>
2019-05-22 20:25:54 +0100

    pequenos updates

--- Trabalhos/GerirTarefas.xaml.cs
+++ Trabalhos/GerirTarefas.xaml.cs
@@ -23,7 +23,7 @@ namespace Trabalhos
     public partial class GerirTarefas : Page
     {
         SqlCommand queryTodasTarefas = new SqlCommand("SELECT Key_Tarefa, Key_Trabalho, Key_Servico, Desconto FROM Tarefa WHERE Key_Trabalho = @KeyTrabalho");
-        SqlCommand queryTodosTempos = new SqlCommand("SELECT Key_Tempo, Key_Tarefa, DataInicio, DataFim FROM Tempo");
+        SqlCommand queryTodosTempos = new SqlCommand("SELECT Key_Tempo, Key_Tarefa, DataInicio, DataFim FROM Tempo WHERE Key_Tarefa = @KeyTarefa");
         SqlCommand queryTodosServicos = new SqlCommand("SELECT Key_Servico, Nome, Preco FROM Servico");
         SqlCommand queryIndexTarefa = new SqlCommand("SELECT Key_Tarefa FROM Tarefa WHERE Key_Tarefa = @KeyTarefa");
         SqlCommand queryIndexTempo = new SqlCommand("SELECT Key_Tempo FROM Tempo WHERE Key_Tempo = @KeyTempo");
@@ -40,6 +40,9 @@ namespace Trabalhos
         List<Servico> servicos = new List<Servico>();
         List<Tarefa> tarefas = new List<Tarefa>();
         List<Tempo> tempos = new List<Tempo>();
+
+        List<ListaTarefas> listaTarefa = new List<ListaTarefas>();
+        List<ListaTempo> listaTempo = new List<ListaTempo>();
 
         //Gerar chave aleatória 
         public static string RandomString(int length)
@@ -59,13 +62,29 @@ namespace Trabalhos
         {
             InitializeComponent();
 
+            Lbl_Trabalho.Content = ("Tarefas do trabalho {0}", InterPages.KeyTrabalho);
+
             LigarBaseDados();
+
+            foreach (Tarefa item in tarefas)
+            {
+                TimeSpan tempoDecorrido = new TimeSpan(0, 0, 0);
+                foreach (Tempo itm in tempos)
+                {
+                    tempoDecorrido += itm.DataFim - itm.DataInicio;
+                }
+
+                listaTarefa.Add(new ListaTarefas { ChaveTarefa = item.ChaveTarefa, Servico = servicos.Find(lst => lst.ChaveServico == item.ChaveServico).Nome, Tempo = tempoDecorrido });
+            }
 
             temporizador.Interval = new TimeSpan(0, 0, 1);
             temporizador.Tick += new EventHandler(Timer_Tick);
 
-            Lst_Tarefas.ItemsSource = servicos;
+            Lst_Tarefas.ItemsSource = listaTarefa;
             Lst_Tarefas.Items.Refresh();
+
+            Cb_Servico.ItemsSource = servicos;
+            Cb_Servico.Items.Refresh();
         }
 
         //Funçoes de butoes e lista
@@ -99,8 +118,6 @@ namespace Trabalhos
         //Botao alterar tarefa selecionada
         private void Btn_AtualizarTarefa_Click(object sender, RoutedEventArgs e)
         {
-            List<ListaTempo> listaTempo = new List<ListaTempo>();
-
             decimal preco = 0;
 
             KeyValido = false;
@@ -112,6 +129,8 @@ namespace Trabalhos
 
             Lbl_CodigoTarefa.Content = tarefas[Lst_Tarefas.SelectedIndex].ChaveTarefa;
             Cb_Servico.SelectedItem = servicos.Find(lista => lista.ChaveServico == tarefas[Lst_Tarefas.SelectedIndex].ChaveServico).Nome;
+
+            listaTempo.Clear();
 
             foreach (Tempo item in tempos)
             {
@@ -129,9 +148,9 @@ namespace Trabalhos
                         tempoDecorrido = new TimeSpan(0, 0, 0);
                     }
 
-                    listaTempo.Add(new ListaTempo { DataInicio = item.DataInicio, DataFim = item.DataFim, TempoDecorrido = tempoDecorrido } );
+                    listaTempo.Add(new ListaTempo { ChaveTempo = item.ChaveTempo, DataInicio = item.DataInicio, DataFim = item.DataFim, TempoDecorrido = tempoDecorrido } );
                 }
-            }
+            }   
 
             Lst_Tempo.ItemsSource = listaTempo;
             Lst_Tempo.Items.Refresh();
@@ -206,6 +225,160 @@ namespace Trabalhos
             }
         }
 
+        //Botao cancelar apagar tarefa
+        private void Btn_CancelarApagar_Click(object sender, RoutedEventArgs e)
+        {
+            Grd_ValidarApagar.Visibility = Visibility.Hidden;
+            BloquearFundo.Visibility = Visibility.Hidden;
+            LimparCampos();
+        }
+
+        //Lista tarefas
+        private void Lst_Tarefas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Lst_Tarefas.SelectedIndex >= 0)
+            {
+                decimal preco = 0;
+                DataBase.conexao = new SqlConnection(DataBase.stringConexao);
+
+                queryTodosTempos.Connection = DataBase.conexao;
+                queryTodasTarefas.Parameters.AddWithValue("@KeyTarefa", tarefas[Lst_Tarefas.SelectedIndex].ChaveTarefa);
+                Reader = queryTodosTempos.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    tempos.Add(new Tempo { ChaveTempo = Convert.ToString(Reader["Key_Tempo"].ToString()), ChaveTarefa = Convert.ToString(Reader["Key_Tarefa"].ToString()), DataInicio = Convert.ToDateTime(Reader["DataIncio"] as DateTime?), DataFim = Convert.ToDateTime(Reader["DataFim"] as DateTime?) });
+                }
+
+                Reader.Close();
+
+                Lbl_CodigoTarefa.Content = tarefas[Lst_Tarefas.SelectedIndex].ChaveTarefa;
+                Cb_Servico.SelectedItem = servicos.Find(lst => lst.ChaveServico == tarefas[Lst_Tarefas.SelectedIndex].ChaveServico).Nome;
+
+                listaTempo.Clear();
+
+                foreach (Tempo item in tempos)
+                {
+                    TimeSpan tempoDecorrido;
+
+                    try
+                    {
+                        tempoDecorrido = item.DataFim - item.DataInicio;
+                        preco += servicos.Find(lst => lst.ChaveServico == tarefas.Find(lstt => lstt.ChaveTarefa == item.ChaveTarefa).ChaveServico).Preco;
+                    }
+                    catch (Exception)
+                    {
+                        tempoDecorrido = new TimeSpan(0, 0, 0);
+                    }
+
+                    listaTempo.Add(new ListaTempo { ChaveTempo = item.ChaveTempo, DataInicio = item.DataInicio, DataFim = item.DataFim, TempoDecorrido = tempoDecorrido });
+                }
+
+                Lst_Tempo.ItemsSource = listaTempo;
+                Lst_Tempo.Items.Refresh();
+
+                Sld_Desconto.Value = Convert.ToDouble(tarefas[Lst_Tarefas.SelectedIndex].Desconto);
+                Lbl_Desconto.Content = ("{0} %", Sld_Desconto.Value);
+                Lbl_Preco.Content = ("{0} €", preco);
+
+                Btn_AtualizarCliente.IsEnabled = true;
+
+                try
+                {
+                    DataBase.conexao.Open();
+                    queryProcurarTrabalhosCliente.Connection = DataBase.conexao;
+                    queryProcurarTrabalhosCliente.Parameters.AddWithValue("@Key_Cliente", clientes[Lst_Clientes.SelectedIndex].ChaveCliente);
+
+                    Reader = queryProcurarTrabalhosCliente.ExecuteReader();
+                    queryProcurarTrabalhosCliente.Parameters.Clear();
+
+                    if (Reader.HasRows)
+                    {
+                        Reader.Read();
+
+                        trabalhosCliente = Convert.ToInt64(Reader["Contagem"].ToString());
+
+                        if (trabalhosCliente > 0)
+                        {
+                            Btn_ApagarCliente.IsEnabled = false;
+                        }
+                        else if (trabalhosCliente == 0)
+                        {
+                            Btn_ApagarCliente.IsEnabled = true;
+                        }
+                    }
+                    else
+                    {
+                        Btn_ApagarCliente.IsEnabled = true;
+                    }
+
+                    Reader.Close();
+                    DataBase.conexao.Close();
+                }
+                catch (Exception ex)
+                {
+                    Lbl_Erros.Text = "Erro Inesperado!\nVerifique a lista de erros conhecidos.\nErro: " + ex;
+                }
+            }
+            else if (Lst_Clientes.SelectedIndex == -1)
+            {
+                Btn_AtualizarCliente.IsEnabled = false;
+                Btn_ApagarCliente.IsEnabled = false;
+            }
+        }
+
+        //Funçoes de validação
+        //Validar nome do cliente
+        private void Tb_NomeCliente_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            temporizador.Stop();
+
+            Tb_NomeCliente.Text = Tb_NomeCliente.Text.TrimStart();
+            char[] nome = Tb_NomeCliente.Text.ToCharArray();
+
+            if (!string.IsNullOrEmpty(Tb_NomeCliente.Text.TrimStart()) && !char.IsUpper(nome[0]))
+            {
+                nome[0] = char.ToUpper(nome[0]);
+                Tb_NomeCliente.Text = new string(nome);
+                Tb_NomeCliente.SelectionStart = Tb_NomeCliente.Text.Length;
+            }
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+
+                if (!ValidarNome.IsMatch(nome[i].ToString()) | (i >= 1 && char.IsWhiteSpace(nome[i]) & char.IsWhiteSpace(nome[i - 1])))
+                {
+                    Tb_NomeCliente.Text = Tb_NomeCliente.Text.Remove(i, 1);
+                    Array.Clear(nome, 0, nome.Length);
+                    nome = Tb_NomeCliente.Text.TrimStart().ToCharArray();
+                    Tb_NomeCliente.SelectionStart = i;
+                }
+            }
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+                if (i > 0 && char.IsWhiteSpace(nome[i - 1]) && !char.IsUpper(nome[i]))
+                {
+                    nome[i] = char.ToUpper(nome[i]);
+                    Tb_NomeCliente.Text = new string(nome);
+                    Tb_NomeCliente.SelectionStart = i + 1;
+                }
+
+                if (i > 0 && char.IsLetter(nome[i - 1]) && char.IsUpper(nome[i]))
+                {
+                    nome[i] = char.ToLower(nome[i]);
+                    Tb_NomeCliente.Text = new string(nome);
+                    Tb_NomeCliente.SelectionStart = i + 1;
+                }
+            }
+
+            NomeValido = false;
+
+            temporizador.Start();
+
+            AtualizarBotoes();
+        }
+
         //Funçoes gerais
         //Ligar com base de dados e ler todos os dados
         void LigarBaseDados()
@@ -222,16 +395,6 @@ namespace Trabalhos
                 while (Reader.Read())
                 {
                     tarefas.Add(new Tarefa { ChaveTarefa = Convert.ToString(Reader["Key_Tarefa"].ToString()), ChaveTrabalho = Convert.ToString(Reader["Key_Trabalho"].ToString()), ChaveServico = Convert.ToInt32(Reader["Key_Servico"].ToString()), Desconto = Convert.ToDecimal(Reader["Desconto"].ToString()) });
-                }
-
-                Reader.Close();
-
-                queryTodosTempos.Connection = DataBase.conexao;
-                Reader = queryTodosTempos.ExecuteReader();
-
-                while (Reader.Read())
-                {
-                    tempos.Add(new Tempo { ChaveTempo = Convert.ToString(Reader["Key_Tempo"].ToString()), ChaveTarefa = Convert.ToString(Reader["Key_Tarefa"].ToString()), DataInicio = Convert.ToDateTime(Reader["DataIncio"] as DateTime?), DataFim = Convert.ToDateTime(Reader["DataFim"] as DateTime?) });
                 }
 
                 Reader.Close();
@@ -343,8 +506,16 @@ namespace Trabalhos
 
     }
 
+    class ListaTarefas
+    {
+        public string ChaveTarefa { get; set; }
+        public string Servico { get; set; }
+        public TimeSpan Tempo { get; set; }
+    }
+
     class ListaTempo
     {
+        public string ChaveTempo { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
         public TimeSpan TempoDecorrido { get; set; }
